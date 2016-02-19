eff8cf34bd969d11469d9a7eb3f5d38e106950f3
Peng Liu <liupeng@imscv.com>
2016-02-19 11:15:12 +0800

    Fix rbm code bug, core: missing batchsize while updating weights.

--- rbm_MNIST_test.py
+++ rbm_MNIST_test.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import input_data
 import Image
 from util import tile_raster_images
@@ -19,23 +20,23 @@ trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images,\
 X = tf.placeholder("float", [None, 784])
 Y = tf.placeholder("float", [None, 10])
 
-rbm_w = tf.Variable(tf.zeros([784, 500]), name="rbm_w")
-rbm_vb = tf.Variable(tf.zeros([784]), name="rbm_vb")
-rbm_hb = tf.Variable(tf.zeros([500]), name="rbm_hb")
-h0 = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb, name="h0"))
+rbm_w = tf.placeholder("float", [784, 500])
+rbm_vb = tf.placeholder("float", [784])
+rbm_hb = tf.placeholder("float", [500])
+h0 = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb))
 v1 = sample_prob(tf.nn.sigmoid(
-    tf.matmul(h0, tf.transpose(rbm_w)) + rbm_vb, name="v1"))
-h1 = tf.nn.sigmoid(tf.matmul(v1, rbm_w) + rbm_hb, name="h1")
+    tf.matmul(h0, tf.transpose(rbm_w)) + rbm_vb))
+h1 = tf.nn.sigmoid(tf.matmul(v1, rbm_w) + rbm_hb)
 w_positive_grad = tf.matmul(tf.transpose(X), h0)
 w_negative_grad = tf.matmul(tf.transpose(v1), h1)
-update_w = rbm_w + alpha * (w_positive_grad - w_negative_grad)
+update_w = rbm_w + alpha * \
+    (w_positive_grad - w_negative_grad) / tf.to_float(tf.shape(X)[0])
 update_vb = rbm_vb + alpha * tf.reduce_mean(X - v1, 0)
 update_hb = rbm_hb + alpha * tf.reduce_mean(h0 - h1, 0)
 
-h = tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb)
-h_sample = tf.nn.relu(tf.sign(h - tf.random_uniform(tf.shape(h))))
-v = tf.nn.sigmoid(tf.matmul(h_sample, tf.transpose(rbm_w)) + rbm_vb)
-v_sample = tf.nn.relu(tf.sign(v - tf.random_uniform(tf.shape(v))))
+h_sample = sample_prob(tf.nn.sigmoid(tf.matmul(X, rbm_w) + rbm_hb))
+v_sample = sample_prob(tf.nn.sigmoid(
+    tf.matmul(h_sample, tf.transpose(rbm_w)) + rbm_vb))
 err = X - v_sample
 err_sum = tf.reduce_mean(err * err)
 
@@ -43,21 +44,30 @@ sess = tf.Session()
 init = tf.initialize_all_variables()
 sess.run(init)
 
-print sess.run(err_sum, feed_dict={X: trX})
+n_w = np.zeros([784, 500], np.float32)
+n_vb = np.zeros([784], np.float32)
+n_hb = np.zeros([500], np.float32)
+o_w = np.zeros([784, 500], np.float32)
+o_vb = np.zeros([784], np.float32)
+o_hb = np.zeros([500], np.float32)
+print sess.run(
+    err_sum, feed_dict={X: trX, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
 
 for start, end in zip(
         range(0, len(trX), batchsize), range(batchsize, len(trX), batchsize)):
     batch = trX[start:end]
-    n_w = sess.run(update_w, feed_dict={X: batch})
-    n_vb = sess.run(update_vb, feed_dict={X: batch})
-    n_hb = sess.run(update_hb, feed_dict={X: batch})
-    sess.run(rbm_w.assign(n_w))
-    sess.run(rbm_vb.assign(n_vb))
-    sess.run(rbm_hb.assign(n_hb))
-    # print "Reconstruction Error is: ", sess.run(err_sum)
+    n_w = sess.run(update_w, feed_dict={
+                   X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    n_vb = sess.run(update_vb, feed_dict={
+                    X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    n_hb = sess.run(update_hb, feed_dict={
+                    X: batch, rbm_w: o_w, rbm_vb: o_vb, rbm_hb: o_hb})
+    o_w = n_w
+    o_vb = n_vb
+    o_hb = n_hb
     if start % 10000 == 0:
-        print sess.run(err_sum, feed_dict={X: trX})
-
+        print sess.run(
+            err_sum, feed_dict={X: trX, rbm_w: n_w, rbm_vb: n_vb, rbm_hb: n_hb})
         image = Image.fromarray(
             tile_raster_images(
                 X=n_w.T,
